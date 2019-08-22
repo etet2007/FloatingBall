a81525dedffa109f46224d3e3459c1984a434784
刘奇天 <liuqitian@atplatform.net>
2019-08-22 15:16:33 +0800

    bug fix in SettingFragment refreshViews

--- app/src/main/java/com/chenyee/stephenlau/floatingball/QuickSettingService.java
+++ app/src/main/java/com/chenyee/stephenlau/floatingball/QuickSettingService.java
@@ -20,82 +20,84 @@ import static com.chenyee.stephenlau.floatingball.util.StaticStringUtil.PREF_IS_
  */
 @TargetApi(Build.VERSION_CODES.N)
 public class QuickSettingService extends TileService {
+    public static final String QUICK_SETTING_REFRESH_MAIN_ACTIVITY_ACTION = "quickSettingRefreshMainActivity";
+    private static final String TAG = QuickSettingService.class.getSimpleName();
 
-  private static final String TAG = QuickSettingService.class.getSimpleName();
-  private boolean isAddedBall = false;
-  private Tile tile;
+    private boolean isAddedBall = false;
+    private Tile tile;
 
-  //当用户从Edit栏添加到快速设定中调用
-  @Override
-  public void onTileAdded() {
-    Log.d(TAG, "onTileAdded");
-  }
-
-  //当用户从快速设定栏中移除的时候调用
-  @Override
-  public void onTileRemoved() {
-    Log.d(TAG, "onTileRemoved");
-  }
-
-
-  // 点击的时候
-  @Override
-  public void onClick() {
-
-    if (isAddedBall) {
-      removeFloatBall();
-    } else {
-      addFloatBall();
+    //当用户从Edit栏添加到快速设定中调用
+    @Override
+    public void onTileAdded() {
+        Log.d(TAG, "onTileAdded");
     }
-    isAddedBall = !isAddedBall;
-    SharedPrefsUtils.setBooleanPreference(PREF_IS_ADDED_BALL_IN_SETTING, isAddedBall);
 
-    LocalBroadcastManager.getInstance(QuickSettingService.this).sendBroadcast(new Intent("refreshActivity"));
-    refreshTile();
-  }
-
-
-  // 打开下拉菜单的时候调用,当快速设置按钮并没有在编辑栏拖到设置栏中不会调用
-  // 在TileAdded之后会调用一次
-  @Override
-  public void onStartListening() {
-    Log.d(TAG, "onStartListening");
-    tile = getQsTile();
-    isAddedBall = SharedPrefsUtils.getBooleanPreference(PREF_IS_ADDED_BALL_IN_SETTING, false);
-    Log.d(TAG, "onStartListening isAddedBall" + isAddedBall);
-
-    refreshTile();
-  }
-
-  // 关闭下拉菜单的时候调用,当快速设置按钮并没有在编辑栏拖到设置栏中不会调用
-  // 在onTileRemoved移除之前也会调用移除
-  @Override
-  public void onStopListening() {
-    Log.d(TAG, "onStopListening");
-  }
-
-  private void addFloatBall() {
-    Intent intent = new Intent(QuickSettingService.this, FloatingBallService.class);
-    Bundle data = new Bundle();
-    data.putInt(EXTRA_TYPE, FloatingBallService.TYPE_START);
-    intent.putExtras(data);
-    startService(intent);
-  }
-
-  private void removeFloatBall() {
-    Intent intent = new Intent(QuickSettingService.this, FloatingBallService.class);
-    Bundle data = new Bundle();
-    data.putInt(EXTRA_TYPE, FloatingBallService.TYPE_REMOVE_ALL);
-    intent.putExtras(data);
-    startService(intent);
-  }
-
-  private void refreshTile() {
-    if (isAddedBall) {
-      tile.setState(Tile.STATE_ACTIVE);
-    } else {
-      tile.setState(Tile.STATE_INACTIVE);
+    //当用户从快速设定栏中移除的时候调用
+    @Override
+    public void onTileRemoved() {
+        Log.d(TAG, "onTileRemoved");
     }
-    tile.updateTile();
-  }
+
+    /**
+     * 打开下拉菜单的时候调用，当快速设置按钮并没有在编辑栏拖到设置栏中不会调用
+     * 在TileAdded之后会调用一次
+     */
+    @Override
+    public void onStartListening() {
+        Log.d(TAG, "onStartListening");
+
+        tile = getQsTile();
+
+        isAddedBall = SharedPrefsUtils.getBooleanPreference(PREF_IS_ADDED_BALL_IN_SETTING, false);
+        Log.d(TAG, "onStartListening isAddedBall:" + isAddedBall);
+
+        refreshTile();
+    }
+
+    @Override
+    public void onClick() {
+        if (isAddedBall) {
+            removeFloatBall();
+        } else {
+            addFloatBall();
+        }
+        isAddedBall = !isAddedBall;
+        SharedPrefsUtils.setBooleanPreference(PREF_IS_ADDED_BALL_IN_SETTING, isAddedBall);
+
+        LocalBroadcastManager.getInstance(QuickSettingService.this).sendBroadcast(new Intent(QUICK_SETTING_REFRESH_MAIN_ACTIVITY_ACTION));
+
+        refreshTile();
+    }
+
+    // 关闭下拉菜单的时候调用,当快速设置按钮并没有在编辑栏拖到设置栏中不会调用
+    // 在onTileRemoved移除之前也会调用移除
+    @Override
+    public void onStopListening() {
+        Log.d(TAG, "onStopListening");
+    }
+
+    private void addFloatBall() {
+        Intent intent = new Intent(QuickSettingService.this, FloatingBallService.class);
+        Bundle data = new Bundle();
+        data.putInt(EXTRA_TYPE, FloatingBallService.TYPE_START);
+        intent.putExtras(data);
+        startService(intent);
+    }
+
+    private void removeFloatBall() {
+        Intent intent = new Intent(QuickSettingService.this, FloatingBallService.class);
+        Bundle data = new Bundle();
+        data.putInt(EXTRA_TYPE, FloatingBallService.TYPE_REMOVE_ALL);
+        intent.putExtras(data);
+        startService(intent);
+    }
+
+    private void refreshTile() {
+        if (isAddedBall) {
+            tile.setState(Tile.STATE_ACTIVE);
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
+        }
+        tile.updateTile();
+    }
 }
